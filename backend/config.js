6aa31af62c536b28c7020309d2d416a0559a3a57
mdnob <mdnobz3@gmail.com>
2025-02-10 01:29:55 -0500

    chore: add .env and dotenv dependency

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const dbConfig = {
-    user: '',
-    password: '',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     connectString: 'oracle.cise.ufl.edu:1521/orcl'
 };
+
+console.log(`Connecting to database with ${dbConfig.user} ${dbConfig.password}`);
 
 module.exports = dbConfig;
